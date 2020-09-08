7ed82c68d9f9f1510693836d2cc4eef579545a33
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2020-09-07 21:36:11 -0400

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios_LibrosCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
